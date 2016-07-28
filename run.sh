0125d94fe1b233f69cbe522a240bdc7f1821b289
Amitoj Setia <asetia@juniper.net>
2016-07-28 14:24:41 -0700

    gui working on osx

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 open -a xquartz
+#brew install socat
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\"
 sudo docker run --net=host --privileged -e DISPLAY=192.168.99.1:0 wireshark
